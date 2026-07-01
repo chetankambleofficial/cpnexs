123b2b27fbbf1dcadea3ca01802ddee0cdd8fba5
Chetan Prakash Kamble <Chetan@BSOLAD.com>
2026-07-01 12:26:19 +0530

    email service error fixed

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  env: {
+    SMTP_HOST: process.env.SMTP_HOST,
+    SMTP_PORT: process.env.SMTP_PORT,
+    SMTP_USER: process.env.SMTP_USER,
+    SMTP_PASS: process.env.SMTP_PASS,
+    CONTACT_FROM_EMAIL: process.env.CONTACT_FROM_EMAIL,
+    CONTACT_TO_EMAIL: process.env.CONTACT_TO_EMAIL,
+  },
 };
 
 export default nextConfig;
